68633f5fce5323949b4ff2b6b8b797f0f22eace7
shibamoto <silverbirder@gmail.com>
2023-03-25 17:49:27 +0900

    fix: use input_schema data

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import {Actor} from 'apify';
-import {PlaywrightCrawler} from 'crawlee';
-import {router, failedRouter} from './routes.js';
+import { Actor } from 'apify';
+import { PlaywrightCrawler } from 'crawlee';
+import { router, failedRouter } from './routes.js';
 
 await Actor.init();
 
-const startUrls = ['https://zenn.dev/silverbirder'];
+const input = await Actor.getInput<{ startUrls: { url: string }[] }>();
+const startUrls = input?.startUrls || ['https://zenn.dev/silverbirder'];
 
-const proxyConfiguration = await Actor.createProxyConfiguration({useApifyProxy: false});
+const proxyConfiguration = await Actor.createProxyConfiguration({ useApifyProxy: false });
 
 const crawler = new PlaywrightCrawler({
     proxyConfiguration,
--- src/routes.ts
+++ src/routes.ts
@@ -1,55 +1,60 @@
-import {Dataset, createPlaywrightRouter} from 'crawlee';
+import { Dataset, createPlaywrightRouter, GlobInput } from 'crawlee';
+import { Actor } from 'apify';
 
 export const router = createPlaywrightRouter();
 
-router.addDefaultHandler(async ({enqueueLinks, log}) => {
+router.addDefaultHandler(async ({ enqueueLinks, log }) => {
     log.info(`enqueueing new URLs`);
+    const input = await Actor.getInput<{ articleGlobs: GlobInput[] }>();
+    const articleGlobs = input?.articleGlobs || ['https://zenn.dev/*/articles/*'];
     await enqueueLinks({
-        globs: ["https://zenn.dev/*/articles/*"],
+        globs: articleGlobs,
         label: 'article',
     });
 });
 
-router.addHandler('article', async ({enqueueLinks, request, page, log}) => {
+router.addHandler('article', async ({ enqueueLinks, request, page, log }) => {
     const title = await page.title();
-    log.info(`article ${title}`, {url: request.loadedUrl});
+    log.info(`article ${title}`, { url: request.loadedUrl });
+    const input = await Actor.getInput<{ ignoreLinks: string[] }>();
+    const ignoreLinks = input?.ignoreLinks || [
+        'https://zenn.dev/',
+        'https://zenn.dev/search',
+        'https://zenn.dev/faq#what-is-publication',
+        'https://zenn.dev/faq#badges',
+        'https://zenn.dev/about',
+        'https://classmethod.jp/',
+        'https://info.zenn.dev/',
+        'https://zenn.dev/zenn',
+        'https://zenn.dev/publications',
+        'https://zenn.dev/faq',
+        'https://twitter.com/zenn_dev',
+        'https://github.com/zenn-dev',
+        'https://zenn.dev/mediakit',
+        'https://zenn.dev/terms',
+        'https://zenn.dev/privacy',
+        'https://zenn.dev/terms/transaction-law',
+        'https://classmethod.connpass.com/event/276869/',
+        'https://zenn.dev/tech-or-idea',
+    ];
     await enqueueLinks({
         label: 'link',
-        strategy: "all",
+        strategy: 'all',
         userData: {
             referer: request.loadedUrl,
         },
         transformRequestFunction: (req) => {
             if (req.url.endsWith('.pdf')) return false;
-            if ([
-                "https://zenn.dev/",
-                'https://zenn.dev/search',
-                'https://zenn.dev/faq#what-is-publication',
-                'https://zenn.dev/faq#badges',
-                'https://zenn.dev/about',
-                'https://classmethod.jp/',
-                'https://info.zenn.dev/',
-                'https://zenn.dev/zenn',
-                'https://zenn.dev/publications',
-                'https://zenn.dev/faq',
-                'https://twitter.com/zenn_dev',
-                'https://github.com/zenn-dev',
-                'https://zenn.dev/mediakit',
-                'https://zenn.dev/terms',
-                'https://zenn.dev/privacy',
-                'https://zenn.dev/terms/transaction-law',
-                'https://classmethod.connpass.com/event/276869/',
-                "https://zenn.dev/tech-or-idea",
-            ].indexOf(req.url) !== -1) return false;
+            if (ignoreLinks.indexOf(req.url) !== -1) return false;
             return req;
         },
     });
 });
 
-router.addHandler('link', async ({request, page, log, response}) => {
+router.addHandler('link', async ({ request, page, log, response }) => {
     const title = await page.title();
     const status = response && response.status();
-    const info = {url: request.loadedUrl, status: status, userData: request.userData};
+    const info = { url: request.loadedUrl, status, userData: request.userData };
     if (status && status >= 400 && status < 500) {
         log.error(`4xx link ${title}`, info);
         await Dataset.pushData(info);
@@ -59,8 +64,8 @@ router.addHandler('link', async ({request, page, log, response}) => {
 });
 
 export const failedRouter = createPlaywrightRouter();
-failedRouter.addHandler('link', async ({request, log}) => {
-    const info = {url: request.url, status: 500, userData: request.userData};
+failedRouter.addHandler('link', async ({ request, log }) => {
+    const info = { url: request.url, status: 500, userData: request.userData };
     log.error(`failed link`, info);
     await Dataset.pushData(info);
 });
